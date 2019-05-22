66dde83f850fe39f2c8c1fc95ca4d15ab7649d98
Erona <erona@loli.bz>
2019-05-22 11:48:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# ubuntu-setup
+a script to setup Ubuntu server
+
+## Usage
+**This script ONLY tested on Ubuntu18.04**
+
+**Please DO NOT run this on other versions or distributions**
+
+Install all items or view available items:
+```bash
+bash <(curl https://raw.githubusercontent.com/Eronana/ubuntu-setup/master/setup.sh)
+```
+
+Install specified items:
+```bash
+bash <(curl https://raw.githubusercontent.com/Eronana/ubuntu-setup/master/setup.sh) tools node ...
+```
---
+++ setup.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+# This script used to install softwares on Ubuntu 18.04 
+# Please DO NOT run it on other OS
+# Usage:
+#   bash <(curl https://raw.githubusercontent.com/Eronana/ubuntu-setup/master/setup.sh)
+#   bash <(curl https://raw.githubusercontent.com/Eronana/ubuntu-setup/master/setup.sh) tools zsh node ...
+
+NODE_LTS_VERSION=10
+ZSH_CONFIG_INSTALL_DIR=/opt
+TOOLS=(
+  git
+  screen
+  language-pack-zh-hans
+)
+NODE_TOOLS=(
+  http-server
+  tldr
+)
+
+function in_array {
+  local e
+  for e in ${@:2}; do 
+    [[ $e == $1 ]] && return 0; 
+  done
+  return 1
+}
+
+function list_array {
+  local e
+  for e in $@; do 
+    echo -e "\033[36m$e\033[0m"
+  done
+}
+
+function install {
+  if [[ $EUID -ne 0 ]]; then
+    echo -e "\033[31mThis script must be run as root.\033[0m"
+    exit 1
+  fi
+
+  local installs=`declare -F | grep install_ | sed 's/^declare -f install_//'`
+  local list=${@-${installs[@]}}
+  local toInstalls=()
+  local name
+
+  for name in $list; do
+    if ! in_array $name ${installs[@]}; then
+      echo -e "\033[31mIgnored invalid item: \033[33m$name\033[0m"
+    else
+      toInstalls+=($name)
+    fi
+  done
+
+  local total=${#toInstalls[@]}
+  local count=0
+  if [ $total -eq 0 ]; then
+    echo -e "\033[31mNo valid install item, available item:\033[0m"
+    echo -e "\033[36m${installs[*]}\033[0m"
+    exit 1
+  fi
+
+  echo "The following items will be installed:"
+  list_array ${toInstalls[@]}
+  read -p "Continue (Y/n)?" CONT
+  echo
+  if [[ ! $CONT =~ ^[Yy]?$ ]]
+  then
+    exit 1
+  fi
+
+  for name in ${toInstalls[@]}; do
+    count=$((count + 1))
+    echo -e "\033[32m========[Installing $name($count/$total)]========\033[0m"
+    $"install_"${name}
+    if [[ $? -ne 0 ]]; then
+      echo -e "\033[31mFailed to install \033[33m$name\033[0m"
+    fi
+  done
+  echo -e "\033[32mDone.\033[0m"
+}
+
+function install_tools {
+  echo "The following Ubuntu packages will be installed:"
+  list_array ${TOOLS[@]} 
+  apt install -y ${TOOLS[*]} 
+}
+
+function install_docker {
+  apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common &&
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - &&
+  apt-key fingerprint 0EBFCD88 &&
+  add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable" &&
+  apt-get install docker-ce docker-ce-cli containerd.io
+}
+
+function install_zsh {
+  apt install -y zsh autojump python3-dev python3-pip python3-setuptools &&
+  pip3 install thefuck &&
+  pushd $ZSH_CONFIG_INSTALL_DIR > /dev/null &&
+  git clone https://github.com/eronana/zsh-config.git &&
+  cd zsh-config &&
+  touch zshrc.zwc &&
+  chmod a+w zshrc.zwc &&
+  echo "source $(pwd)/zshrc.linux" >> ~/.zshrc &&
+  chsh -s $(which zsh) &&
+  popd > /dev/null
+}
+
+function install_node {
+  curl -sL https://deb.nodesource.com/setup_$NODE_LTS_VERSION.x | bash - &&
+  apt-get install -y nodejs
+}
+
+function install_node_tools {
+  echo "The following node packages will be installed:"
+  list_array ${NODE_TOOLS[@]} 
+  npm i -g --registry=https://registry.npm.taobao.org ${NODE_TOOLS[*]} 
+}
+
+function install_test {
+  echo "test test test"
+}
+
+function install_test2 {
+  echo "test22222222222222"
+}
+
+install $*
+
